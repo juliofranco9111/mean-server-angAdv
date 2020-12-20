066d9f9068f846be81aba60aafbef228abc85957
axlmayhem <julio.franco9111@gmail.com>
2020-12-20 04:27:01 -0300

    SPA servida

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // dotenv sirve para crear facilmente variables de entorno,
 // basta con definirlas en el archivo .env
 require ('dotenv').config();
+const path = require('path')
 
 const express = require('express');
 
 const cors = require('cors')
 
 const { dbConnection } = require('./database/config');
+
 
 
 //Crear el servidor de express
@@ -32,6 +34,10 @@ app.use( '/api/medicos', require('./routes/medicos') );
 app.use( '/api/login', require('./routes/auth') );
 app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/upload', require('./routes/uploads') );
+
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve(__dirname, 'public/index.html') );
+});
 
 
 
